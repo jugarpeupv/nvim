2372d06d7be1c03df144b8d2dbd07af9d6811ccd
Julio Garcia <jugarpeupv@gmail.com>
2024-07-12 18:02:08 +0200

    working 2

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -6,7 +6,7 @@ return {
   { "nvim-lua/plenary.nvim",           event = "VeryLazy" },
   { "tpope/vim-surround",              event = { "BufReadPost", "BufNewFile" } },
   { "windwp/nvim-ts-autotag",          after = "nvim-treesitter",              ft = "html" },
-  { "tpope/vim-fugitive",              event = { "BufReadPost", "BufNewFile" } },
+  { "tpope/vim-fugitive",              event = { "VeryLazy" } },
   { "tpope/vim-dispatch",              event = { "BufReadPost", "BufNewFile" } },
   { "kkharji/sqlite.lua",              event = {  "BufReadPost", "BufNewFile"  } },
   { "stsewd/fzf-checkout.vim",         event = { "VeryLazy" } },
